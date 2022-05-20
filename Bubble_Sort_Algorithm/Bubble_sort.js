d3ff565e95cee371eeeb640c34cc3a2715410790
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-05-20 20:58:32 +0200

    Bubble_Sort_Algorithm

---
+++ Bubble_Sort_Algorithm/Bubble_sort.js
@@ -0,0 +1,27 @@
+const swap = (arr,firstIndex,secondIndex)=>{
+    let temp = arr[firstIndex] ;
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+}
+const bubble_sort=(arr)=>{
+    for (let i = 0 ; i <arr.length-1;i++){
+        for(let j = arr.length-1; j>0+i ;j--){
+            if(arr[j] < arr[j-1]){
+                swap(arr,j,j-1)
+            }
+        }
+    }
+    return arr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
