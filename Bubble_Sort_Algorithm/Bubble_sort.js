c3a7dc6988a97c7b1dadace7aecc342519cd4e86
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-05-21 17:02:44 +0200

    Bubble_Sort_Algorithm_Updated_the_loop_counter

--- Bubble_Sort_Algorithm/Bubble_sort.js
+++ Bubble_Sort_Algorithm/Bubble_sort.js
@@ -5,7 +5,7 @@ const swap = (arr,firstIndex,secondIndex)=>{
 }
 const bubble_sort=(arr)=>{
     for (let i = 0 ; i <arr.length-1;i++){
-        for(let j = arr.length-1; j>0+i ;j--){
+        for(let j = arr.length-1; j>i;j--){
             if(arr[j] < arr[j-1]){
                 swap(arr,j,j-1)
             }
