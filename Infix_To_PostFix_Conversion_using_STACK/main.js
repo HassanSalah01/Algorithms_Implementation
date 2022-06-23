e7fcbdf000491fe14cf6c9a328de5846868cdb53
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-06-23 21:20:54 +0200

    Infix_TO_PostFix_Converstion_Algorithm_Using_Stack

--- Infix_To_PostFix_Conversion_using_STACK/main.js
+++ Infix_To_PostFix_Conversion_using_STACK/main.js
@@ -18,11 +18,11 @@ const pres=(x)=>{
 
 function main(){
     let s = new Stack();
-    const exp = "(2+3)/5";
+    const exp ="(2+3)" 
     let newExp= "";
     for (let i = 0 ; i < exp.length;i++){
         if(pres(exp[i])!=0){
-            if(s.length()==0 || s.getPeek()=="(" ){
+            if(s.getLength()==0 || s.getPeek()=="(" ){
                 s.push(exp[i])
             }else if(exp[i]=="("){
                 s.push(exp[i])
@@ -32,10 +32,20 @@ function main(){
                     s.pop();
                 }
                 s.pop();
-            }
+            }else if(pres(exp[i])>pres[s.getPeek]){
+                s.push(exp[i])
+            }else if(pres(exp[i])<pres[s.getPeek]){
+                newExp+=exp[i]
+                s.pop()
+                while(pres(exp[i])<pres[s.getPeek]){
+                    newExp+=exp[i];
+                    s.pop()
+                }
         }else{
             newExp+=exp[i];
         }
     }
+    console.log(newExp);
 
 }
+console.log(pres("/")<pres("+"));
